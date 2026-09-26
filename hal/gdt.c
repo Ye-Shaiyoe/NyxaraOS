5da204e82385b86d9dec3093d35ec89977eae8d3
akrom <akrommuhammadyusuf@gmail.com>
2026-09-26 20:36:37 +0700

    Fixing TSS and ltr descriptors

--- hal/gdt.c
+++ hal/gdt.c
@@ -26,9 +26,9 @@ void tss_set_stack(uint32_t ss0, uint32_t esp0) {
 
 static void write_tss(int32_t num, uint16_t ss0, uint32_t esp0) {
     uint32_t base = (uint32_t)&tss_entry;
-    uint32_t limit = sizeof(tss_entry_t);
+    uint32_t limit = sizeof(tss_entry_t) - 1;
 
-    gdt_set_gate(num, base, limit, 0xE9, 0x00);
+    gdt_set_gate(num, base, limit, 0x89, 0x00);
 
     memset(&tss_entry, 0, sizeof(tss_entry_t));
     tss_entry.ss0  = ss0;
